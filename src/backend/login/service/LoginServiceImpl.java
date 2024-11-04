bb566029b86799aaad811ae2a5e14db615b72593
pyc-chan <rtrtoioil@gmail.com>
2024-11-04 12:19:57 +0900

    Update LoginServiceImpl.java

--- src/backend/login/service/LoginServiceImpl.java
+++ src/backend/login/service/LoginServiceImpl.java
@@ -1,11 +1,12 @@
-package com.ict.project.service;
+package com.ict.project.login.service;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.ict.project.dao.LoginDAO;
-import com.ict.project.vo.LoginVO;
+import com.ict.project.login.dao.LoginDAO;
+import com.ict.project.login.vo.LoginVO;
 
 @Service
 public class LoginServiceImpl implements LoginService{
@@ -14,10 +15,11 @@ public class LoginServiceImpl implements LoginService{
 	private LoginDAO loginDAO;
 	
 	@Override
-	public int loginOK() {
-		return 0;
+	public LoginVO loginOK(String u_id) {
+		return loginDAO.loginOK(u_id);
 	}
 
+	
 	@Override
 	public int joinOK() {
 		return 0;
@@ -25,8 +27,36 @@ public class LoginServiceImpl implements LoginService{
 
 	@Override
 	public int joinOK(LoginVO lvo) {
-		
 		return loginDAO.joinOK(lvo);
+	}
+
+	@Override
+	public String idChk(String u_id) {
+		return loginDAO.idChk(u_id);
+	}
+
+	@Override
+	public int getList() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDetail() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public List<String> getId(String u_em) {
+		return loginDAO.getId(u_em);
+	}
+
+
+	@Override
+	public LoginVO pwFind(LoginVO lvo) {
+		return loginDAO.pwFind(lvo);
 	}
 
 }
