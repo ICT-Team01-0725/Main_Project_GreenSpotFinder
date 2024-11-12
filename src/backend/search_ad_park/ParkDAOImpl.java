bf4af9f1ed5a06040e1a1099571c470f169c9222
hyeongjook <hyeongjoo1008@gmail.com>
2024-11-12 16:51:10 +0900

    Update ParkDAOImpl.java

--- src/backend/search_ad_park/ParkDAOImpl.java
+++ src/backend/search_ad_park/ParkDAOImpl.java
@@ -1,8 +1,6 @@
 package com.ict.edu.parkdao;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,21 +11,25 @@ import com.ict.edu.parkvo.ParkVO;
 @Repository
 public class ParkDAOImpl implements ParkDAO {
 	
-	   @Autowired
+	  @Autowired
 	    private SqlSessionTemplate sqlSessionTemplate;
 
 	    // 공원 검색 (p_ad로 검색)
 	    @Override
 	    public List<ParkVO> searchParks(String p_ad) {
-	        Map<String, Object> params = new HashMap<>();
-	        params.put("p_ad", "%" + p_ad + "%");
-
-	        return sqlSessionTemplate.selectList("park-mapper.searchParks", params);
+	        return sqlSessionTemplate.selectList("park-mapper.searchParks", p_ad);
 	    }
 
 	    // 기본 공원 리스트 조회 (가나다순 4개)
 	    @Override
 	    public List<ParkVO> getTop4Parks() {
-	        return sqlSessionTemplate.selectList("park-mapper.getTop4Parks");
+	    	System.out.println("데이터2");
+	    	return sqlSessionTemplate.selectList("park-mapper.getTop4Parks");
+	    }
+
+	    // 지역별 공원 목록 조회
+	    @Override
+	    public List<ParkVO> getParksByRegion(String region) {
+	        return sqlSessionTemplate.selectList("park-mapper.getParksByRegion", region);
+	    }
 	}
-}
