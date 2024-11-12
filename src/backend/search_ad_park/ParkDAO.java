d42250b76fb447159bbef9c54dd212a075147df1
hyeongjook <hyeongjoo1008@gmail.com>
2024-11-12 16:52:43 +0900

    Update ParkDAO.java

--- src/backend/search_ad_park/ParkDAO.java
+++ src/backend/search_ad_park/ParkDAO.java
@@ -6,7 +6,7 @@ import com.ict.edu.parkvo.ParkVO;
 
 public interface ParkDAO {
     // 주소로 공원 검색
-    List<ParkVO> searchParks(String p_ad);
+    List<ParkVO> searchParksByAddress(String p_ad);
 
     // 기본 공원 리스트 조회
     List<ParkVO> getTop4Parks();
